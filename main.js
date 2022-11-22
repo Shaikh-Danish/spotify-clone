1848b61c05a9e3db3f51a2d9a7527d1d0349374a
danish <shaikhsaalim327@gmail.com>
2022-11-23 00:17:00 +0530

    testing spotify login

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
-/*document.querySelector('.spotify').addEventListener('click', function() {
+
+document.getElementById('log-in').addEventListener('click', function() {
 		
 		var client_id = '1360b7537f904ce898c837bb43690dcf';
-		var redirect_uri = 'http://localhost:8888/callback/';
+		var redirect_uri = 'http://localhost:8000/callback/';
 		
-		
+		/*
 		var client_id = '6db36a01e85845119836d789ac6c1e61';
 		var redirect_uri = 'https://shaikh-danish.github.io/spotify-clone/';
+		*/
 
 		var scope = 'user-read-private user-read-email';
 
@@ -14,6 +16,33 @@
 		url += '&client_id=' + 				encodeURIComponent(client_id);
 		url += '&scope=' + encodeURIComponent(scope);
 		url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
-		window.open(url)
+		/*query = window.open(url);
+		console.log(query);*/
+		this.href = url;
+		console.log(url)
+		this.textContent = url;
 });
-*/
+
+const hamburger = document.getElementById('hamburger-btn')
+
+hamburger.addEventListener('click', function() {
+/*
+		let isOpened = hamburger.getAttribute('aria-expanded');
+		
+		if (isOpened === 'false')
+		{
+				hamburger.ariaExpanded = 'true';
+				//prevent background scrolling
+				document.body.style.overflowY = 'hidden';
+		}
+		else {
+				hamburger.ariaExpanded = 'false';
+				//enable scrolling
+				document.body.style.overflowY = 'visible';
+		}
+		*/
+		
+		hamburger.ariaExpanded = hamburger.ariaExpanded === 'false' ? 'true' : 'false';
+		
+		document.body.style.overflowY = hamburger.ariaExpanded === 'false' ? 'visible' : 'hidden';
+});
