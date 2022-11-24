8ca917d9e5aabbe6ae1775d380a6419a583433b4
danish <shaikhsaalim327@gmail.com>
2022-11-24 23:14:20 +0530

    update

--- main.js
+++ main.js
@@ -1,22 +1,25 @@
+'use strict';
 
-document.getElementById('log-in').addEventListener('click', function() {
-		var client_id = '6db36a01e85845119836d789ac6c1e61';
-		var redirect_uri = 'https://shaikh-danish.github.io/spotify-clone/';
-		var scope = 'user-read-private user-read-email';
+const hamburger = document.getElementById('hamburger-btn');
+const clientId = '6db36a01e85845119836d789ac6c1e61';
+const redirectUri = 'https://shaikh-danish.github.io/spotify-clone/';
+const scope = 'user-read-private user-read-email';
 
-		var url = 'https://accounts.spotify.com/authorize';
+function spotifyLogIn() {
+		let url = 'https://accounts.spotify.com/authorize';
 		url += '?response_type=token';
-		url += '&client_id=' + 				encodeURIComponent(client_id);
+		url += '&client_id=' + 				encodeURIComponent(clientId);
 		url += '&scope=' + encodeURIComponent(scope);
-		url += '&redirect_uri=' + encodeURIComponent(redirect_uri);
+		url += '&redirect_uri=' + encodeURIComponent(redirectUri);
 		window.location = url;
-		console.log(window.location.href)
-});
+		console.log(window.location.search);
+		document.body.onload = function() {
+				this.textContent = url;
+		}
+}
 
-const hamburger = document.getElementById('hamburger-btn')
-
-hamburger.addEventListener('click', function() {
-/*
+function openNavMenu() {
+		/*
 		let isOpened = hamburger.getAttribute('aria-expanded');
 		
 		if (isOpened === 'false')
@@ -35,4 +38,18 @@ hamburger.addEventListener('click', function() {
 		hamburger.ariaExpanded = hamburger.ariaExpanded === 'false' ? 'true' : 'false';
 		
 		document.body.style.overflowY = hamburger.ariaExpanded === 'false' ? 'visible' : 'hidden';
-});
+} 
+
+document.getElementById('log-in').addEventListener('click', spotifyLogIn);
+
+hamburger.addEventListener('click', openNavMenu);
+
+
+const getGenres = async (token) => {
+		const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
+		method: 'GET',
+  headers: { 'Authorization' : 'Bearer ' + token}
+  });
+  const data = await result.json();
+  return data.categories.items;
+}
