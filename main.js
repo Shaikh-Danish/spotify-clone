ca6d11cb379ae7037cc2c3fc4f02fecbcbd3abe5
danish <shaikhsaalim327@gmail.com>
2022-12-02 05:52:13 +0530

    test

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const APP_URL = 'https://shaikh-danish.github.io/spotify-clone';
 const CLIENT_ID = '6db36a01e85845119836d789ac6c1e61';
 
 const REDIRECT_URI = `${APP_URL}`;
-const scope = 'user-read-private user-read-email';
+const scope = 'user-read-private user-read-email playlist-read-private user-follow-read user-library-read user-top-read user-read-recently-played user-library-read user-read-playback-state playlist-read-collaborative user-read-recently-played';
 const TOKEN_PARAM_KEYS = ['access_token', 'token_type', 'expires_in'];
 let logIn = true;
 
