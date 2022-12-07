a8a055c9116de32f7012420633d3ead620aa77a1
danish <shaikhsaalim327@gmail.com>
2022-12-07 18:47:37 +0530

    test

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -6,7 +6,7 @@ const logOutBtn = document.getElementById('log-out');
 const ACCESS_TOKEN_KEY = "access_token";
 const TOKEN_TYPE_KEY = "token_type";
 const EXPIRES_IN_KEY = "expires_in";
-const APP_URL = "http://localhost:8080";
+const APP_URL = "https://shaikh-danish.github.io/spotify-clone";
 
 const BASE_API_URL = "https://api.spotify.com/v1";
 const ENDPOINT = {
